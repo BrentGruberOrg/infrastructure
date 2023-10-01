ca74a6f64dc2c0254b66775ab552dcc2eeb31f4e
BrentGruber <brent.gruber77@gmail.com>
2023-10-01 19:27:09 -0400

    add ip output

--- oracle/outputs.tf
+++ oracle/outputs.tf
@@ -1,3 +1,7 @@
 output "all-availability-domains-in-your-tenancy" {
   value = data.oci_identity_availability_domains.this.availability_domains
 }
+
+output "public-ip" {
+    value = oci_core_public_ip.this.ip_address
+}
