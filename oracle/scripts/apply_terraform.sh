69b283f502a210b903b0c14b26c28268fc06b016
BrentGruber <brent.gruber77@gmail.com>
2023-10-01 18:07:12 -0400

    Fixing shell script

--- oracle/scripts/apply_terraform.sh
+++ oracle/scripts/apply_terraform.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 
-run=true
-
 doppler run terraform init
 
-while ["$run" = true]; do
-
-if ![doppler run --command "terraform apply -auto-approve" | grep -q 'Out of host capacity']; then
-    run=false
-fi
-
-sleep 10
+until ! doppler run --command "terraform apply -auto-approve" | grep -q 'Out of host capacity'
+do
+    sleep 10
 done
