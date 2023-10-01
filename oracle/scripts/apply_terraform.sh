6aa9209031cafa5dc40eb8d4a048a6ecee7b87ad
BrentGruber <brent.gruber77@gmail.com>
2023-10-01 19:19:24 -0400

    Retry script working

--- oracle/scripts/apply_terraform.sh
+++ oracle/scripts/apply_terraform.sh
@@ -3,7 +3,19 @@
 
 doppler run terraform init
 
-until ! doppler run --command "terraform apply -auto-approve" | grep -q 'Out of host capacity'
+keep_running="run"
+
+while [ "$keep_running" = "run" ]
 do
-    sleep 10
+    doppler run --command "terraform apply -auto-approve" | tee terraform.log
+
+    # if 'Out of host capacity' is not in the log, then stop running
+    if ! grep -q 'Out of host capacity' terraform.log; then
+        keep_running="stop"
+    else
+        echo "Failed with out of host capacity error, retrying...."
+        sleep 10
+    fi
+
+    echo $keep_running
 done
